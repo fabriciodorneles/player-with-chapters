633f1b8e62b053d1ce89a04b9297b7a9bcbdb7f4
Fabricio Dorneles <fabricio.dorneles-contractor@procore.com>
2024-11-29 19:13:54 -0300

    finishing components

--- src/components/Module.tsx
+++ src/components/Module.tsx
@@ -1,16 +1,22 @@
 import { ChevronDown } from "lucide-react";
 import { Lesson } from "./Lesson";
 
-export const Module = () => {
+interface ModuleProps {
+  moduleIndex: number;
+  title: string;
+  lessonsAmount: number;
+}
+
+export const Module = ({ moduleIndex, title, lessonsAmount }: ModuleProps) => {
   return (
     <div>
       <button className="flex w-full items-center gap-3 bg-zinc-800 p-4">
         <div className="flex h-10 w-10 rounded-full items-center justify-center bg-zinc-950 text-sm">
-          1
+          {moduleIndex + 1}
         </div>
         <div className="flex flex-col gap-1 text-left ">
-          <strong className="text-sm">Desvendando o Redux</strong>
-          <span className="text-sm text-zinc-400">2 Aulas</span>
+          <strong className="text-sm">{title}</strong>
+          <span className="text-sm text-zinc-400">{`${lessonsAmount} Aulas`}</span>
         </div>
         <ChevronDown className="w-5 h-5 ml-auto text-zinc-400" />
       </button>
